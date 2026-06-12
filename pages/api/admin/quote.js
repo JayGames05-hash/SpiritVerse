8aaba657a555cb22e8a883ba6f623ee6912d0361
JayGames05 <your_github_email@example.com>
2026-06-12 08:35:51 -0400

    Remove duplicate reactions.js and fix import paths in admin/quote.js

--- pages/api/admin/quote.js
+++ pages/api/admin/quote.js
@@ -1,5 +1,5 @@
-import { getUserFromRequest } from '../../lib/auth'
-import readings from '../../data/readings'
+import { getUserFromRequest } from '../../../lib/auth'
+import readings from '../../../data/readings'
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
